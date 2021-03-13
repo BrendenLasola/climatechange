09e7bb856fe02c44e1b74a50f616c8010877697c
Brenden Lasola <77668770+YungHiro@users.noreply.github.com>
2021-03-12 23:52:06 -0800

    updates

---
+++ A4_server.R
@@ -0,0 +1,15 @@
+
+
+server <- function(input,output) {
+  
+  output$plot_data <- renderPlot({
+    
+    colnames(na_co2_data) <- c("Year", "Cement CO2", "Coal CO2", "Gas CO2", "Oil CO2", "Flaring CO2",
+                               "Other Industry CO2")
+    co2_plot <- ggplot(na_co2_data) + 
+      geom_line(aes(x = `Year` , y = na_co2_data[[input$emmission_type]]), col = input$color_var) +
+      labs(title = "Types of Emmision", x = "Year", y = input$nutrition_type)
+    
+    co2_plot
+  })
+}
